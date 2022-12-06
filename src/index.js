8ba4576398d93c74c7e445d8ab61d80ed7efe295
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-12-06 23:20:01 +0200

    add refs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+
+import API from './fetchCountries';
+
+const inputSearch = document.querySelector('#search-box');
+const listCountry = document.querySelector('.country-list');
+const infoAboutCountry = document.querySelector('.country-info');
+
+inputSearch.addEventListener('input', onInputSearch);
+
+
+function onInputSearch(e) {
+    e.preventDefault();
+
+
+}
+
+
+
+// function renderCountryList(name) {
+//   const markup = listCountryCard(name);
+//   listCountry.innerHTML = markup;
+// }
+
+
+//     function fetchCountries(name) {
+//         const urlAPI = `https://restcountries.com/v3.1/name/${name}`
+//         fetch(urlAPI)
+//             .then(response => response.json())
+//             .then(({list})=> {
+//                 render(list)
+//                     .catch(error => console.log(error));
+//     })
+//     }
+
+// function render(list) {
+//     listCountry.innerHTML = '';
+//     list.forEach(({name}) => {
+//         const listEl = `<li>
+//         <p class="country-name"${name}<p/>
+//        <li/>;
+//        ` 
+//         listCountry.insertAdjacentHTML('beforeend', listEl);
+//     }); 
+    
+//     }
+
+
