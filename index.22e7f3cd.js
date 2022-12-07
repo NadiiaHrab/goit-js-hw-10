6c9b0002a4020919c3bffa0e984580f11e69f77b
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-12-07 18:10:32 +0000

    Deploying to gh-pages from @ DemouserNG/goit-js-hw-10@a911471761f358eb8c32aed22a0c476e3914cbec 🚀

--- index.22e7f3cd.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(t){t.preventDefault();e.value}))}();
-//# sourceMappingURL=index.22e7f3cd.js.map
