2b5c8537f4239e8e3d5c083f15f211f2561d6d4a
DemouserNG <108471967+NadiiaHrab@users.noreply.github.com>
2023-01-12 00:44:32 +0200

    filmoteka

--- src/index.js
+++ src/index.js
@@ -1,70 +1,129 @@
-import './css/styles.css';
-import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import API from './fetchCountries';
-import { renderCountryList, renderCountryCard } from "./render-function";
+// import './css/styles.css';
+// import debounce from 'lodash.debounce';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import API from './fetchCountries';
+// import { renderCountryList, renderCountryCard } from "./render-function";
 
 
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
 
-const inputSearch = document.querySelector('#search-box');
-const listCountry = document.querySelector('.country-list');
-const cardCountry = document.querySelector('.country-info');
+// const inputSearch = document.querySelector('#search-box');
+// const listCountry = document.querySelector('.country-list');
+// const cardCountry = document.querySelector('.country-info');
 
-inputSearch.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+// inputSearch.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
 
-function onInputSearch(e) {
-    e.preventDefault();
+// function onInputSearch(e) {
+//     e.preventDefault();
 
-    const query = inputSearch.value.trim();
+//     const query = inputSearch.value.trim();
     
     
 
-    API.fetchCountries(query)
-        .then(onfetchSearch)
-        .catch(onfetchError); 
-};
+//     API.fetchCountries(query)
+//         .then(onfetchSearch)
+//         .catch(onfetchError); 
+// };
 
-function onfetchSearch(countries) {
+// function onfetchSearch(countries) {
 
-     if (countries.length > 10) {
+//      if (countries.length > 10) {
                 
-                Notify.info("Too many matches found. Please enter a more specific name.");
-                onClearRender();
+//                 Notify.info("Too many matches found. Please enter a more specific name.");
+//                 onClearRender();
 
-            } else if (countries.length >= 2 && countries.length <= 10) {
+//             } else if (countries.length >= 2 && countries.length <= 10) {
 
-                renderList(countries);
-                cardCountry.innerHTML = '';
+//                 renderList(countries);
+//                 cardCountry.innerHTML = '';
         
-            } else if (countries.length === 1) {
+//             } else if (countries.length === 1) {
                 
-                renderCard(countries);
-                listCountry.innerHTML = '';
-    }
-};
+//                 renderCard(countries);
+//                 listCountry.innerHTML = '';
+//     }
+// };
 
 
-function renderList(countries) {
-    const list = renderCountryList(countries);
-    listCountry.insertAdjacentHTML('beforeend', list);
-}
+// function renderList(countries) {
+//     const list = renderCountryList(countries);
+//     listCountry.insertAdjacentHTML('beforeend', list);
+// }
 
-function renderCard(countries) {
-    const card = renderCountryCard(countries);
-    cardCountry.insertAdjacentHTML('beforeend', card);
+// function renderCard(countries) {
+//     const card = renderCountryCard(countries);
+//     cardCountry.insertAdjacentHTML('beforeend', card);
     
-}
+// }
 
-function onfetchError(error) {
-    Notify.failure("Oops, there is no country with that name");
-    onClearRender();
+// function onfetchError(error) {
+//     Notify.failure("Oops, there is no country with that name");
+//     onClearRender();
     
+// }
+
+// function onClearRender() {
+//     listCountry.innerHTML = '';
+//     cardCountry.innerHTML = '';
+// };
+
+
+import json from './genres/genres.json';
+
+
+const ul = document.querySelector('.trending__collection');
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const TRENDING = 'trending/movie/week';
+const API_KEY = '1d8f1c2313e3ed4d118cc85bb96261b9';
+
+const genresJson = json.genres;
+console.log(genresJson);
+
+
+const el = genresJson.map(genre => {
+    return genre.name;
+    
+    
+
+})
+    console.log(el);
+
+
+
+    getFetchTrending()
+        .then(renderTrendingCard)
+        .catch((error) => console.log(error));
+
+function getFetchTrending() {
+       return fetch(`${BASE_URL}${TRENDING}?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`)
+        .then(response =>
+        response.json()
+        )
+        .then(data => data.results)
 }
 
-function onClearRender() {
-    listCountry.innerHTML = '';
-    cardCountry.innerHTML = '';
+function renderTrendingCard(films) {
+
+    const cardEl = films.map(film => {
+        return `<li class="trending__item">
+    <img class=" card__img" src="https://www.themoviedb.org/t/p/w500${film.poster_path}"
+      onerror="this.onerror=null;this.src='https://subscribenow.com.au/time/asia/Solo/Content/Images/noCover.gif'" loading="lazy"
+      alt="${film.title}" title="${film.title}" data-id="${film.id}" width="280"/>
+<h3 class="card__title">${film.title}</h3>
+<div class="card-field">
+    <p class="text__vote">${film.release_date}</p>
+    <p class="text__vote">${film.genre_ids}</p>
+</div>
+
+</li>`
+        
+    }).join('');
+
+    ul.insertAdjacentHTML('beforeend', cardEl);
+
+
+    // return cardEl;
 };
